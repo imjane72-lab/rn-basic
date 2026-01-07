494e24fcb23d57301a163c2bd86003bf63893121
imjane72-lab <imjane72@gmail.com>
2026-01-07 09:21:30 +0900

    Initial commit

---
+++ app/index.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+	return (
+		<View className="flex-1 items-center justify-center bg-black">
+			<Text className="text-2xl text-white">나는 지혜입니다.</Text>
+		</View>
+	);
+}
